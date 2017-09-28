c8e9518cf218898101712757f434bb85393ca52f
AboodXD <aboood40091@gmail.com>
2017-09-28 15:13:44 +0400

    Upload the actual thing

---
+++ run-python34.bat
@@ -0,0 +1,18 @@
+@echo OFF
+REM run-python34.bat
+REM
+REM Thanks to LifeMushroom for the idea.
+REM
+REM This only works if:
+REM - you have Python 3.4
+REM - you installed Python 3.4 to the default location
+REM - you installed all the other prerequisites (see readme.txt)
+REM - this file is in the same folder as reggie.py
+REM - you're on Windows
+REM
+REM Usage: Double-click this to run Reggie from source.
+REM
+@echo ON
+
+C:/Python34/python.exe reggie.py
+pause
