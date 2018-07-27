d3670d39bb17192230874b72beb732b5bb38a5a3
Luke <broccolutje@gmail.com>
2018-07-27 22:51:11 +0200

    Go back to python 3.4 syntax

--- reggie.py
+++ reggie.py
@@ -12035,7 +12035,7 @@ class SpriteEditorWidget(QtWidgets.QWidget):
                 nf = SpriteEditorWidget.ExternalPropertyDecoder(f[1], f[2], f[3], f[4], f[5], f[6], f[7], f[8], layout, row, self)
 
             elif f[0] == 7:
-                nf = SpriteEditorWidget.MultiDualboxPropertyDecoder(*f[1:], layout, row, self)
+                nf = SpriteEditorWidget.MultiDualboxPropertyDecoder(f[1], f[2], f[3], f[4], f[5], f[6], f[7], f[8], layout, row, self)
 
             nf.updateData.connect(self.HandleFieldUpdate)
             fields.append(nf)
