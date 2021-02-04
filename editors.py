NSMBW-Community/Reggie-Next

commit 2ab2c86986d51e4417366ccc881aceb6a776a004

Preserve ordering of entrance types. Also changes the default ordering to move the most used types to the top.

misc.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtCore, QtWidgets, QtGui
+import collections
 import sys
 import os
 from xml.etree import ElementTree
@@ -1053,31 +1054,22 @@ def LoadEntranceNames(reload_=False):
     """
     Ensures that the entrance names are loaded
     """
-    # global EntranceTypeNames
     if (globals_.EntranceTypeNames is not None) and not reload_: return
 
     paths, isPatch = globals_.gamedef.recursiveFiles('entrancetypes', True)
     if isPatch:
-        new = [globals_.trans.files['entrancetypes']]
-        for path in paths: new.append(path)
-        paths = new
+        paths = [globals_.trans.files['entrancetypes']] + paths
 
-    NameList = {}
+    names = collections.OrderedDict()
     for path in paths:
-        newNames = {}
         with open(path, 'r') as f:
             for line in f.readlines():
-                id_ = int(line.split(':')[0])
-                newNames[id_] = line.split(':')[1].replace('\n', '')
-
-        for idx in newNames:
-            NameList[idx] = newNames[idx]
+                id_, name = line.strip().split(':')
+                names[int(id_)] = name
 
     globals_.EntranceTypeNames = []
-    idx = 0
-    while idx in NameList:
-        globals_.EntranceTypeNames.append(globals_.trans.string('EntranceDataEditor', 28, '[id]', idx, '[name]', NameList[idx]))
-        idx += 1
+    for idx in names:
+        globals_.EntranceTypeNames.append(globals_.trans.string('EntranceDataEditor', 28, '[id]', idx, '[name]', names[idx]))
 
 
 def LoadTilesetInfo(reload_=False):

commit 0e9c0fe46d3c070c71219f1aebc7b4a530073e87

Fix bug with entrance type not saved properly. 2ab2c86 caused the assumption that the index of the entrance type equals the entrance type value to break. This updates the places where this violation caused problems.

editors.py
@@ -29,7 +29,7 @@ class EntranceEditorWidget(QtWidgets.QWidget):
         self.entranceID.valueChanged.connect(self.HandleEntranceIDChanged)
 
         self.entranceType = QtWidgets.QComboBox()
-        self.entranceType.addItems(globals_.EntranceTypeNames)
+        self.entranceType.addItems(globals_.EntranceTypeNames.values())
         self.entranceType.setToolTip(globals_.trans.string('EntranceDataEditor', 3))
         self.entranceType.activated.connect(self.HandleEntranceTypeChanged)
 
@@ -177,10 +177,12 @@ class EntranceEditorWidget(QtWidgets.QWidget):
         self.ent.UpdateListItem()
         self.editingLabel.setText(globals_.trans.string('EntranceDataEditor', 23, '[id]', i))
 
-    def HandleEntranceTypeChanged(self, i):
+    def HandleEntranceTypeChanged(self, new_index):
         """
         Handler for the entrance type changing
         """
+        i = list(globals_.EntranceTypeNames)[new_index]
+
         self.connectedPipeCheckbox.setVisible(i in self.CanUseFlag8)
         self.connectedPipeReverseCheckbox.setVisible(i in self.CanUseFlag8 and ((self.ent.entsettings & 8) != 0))
         self.pathIDLabel.setVisible(i and ((self.ent.entsettings & 8) != 0))

misc.py
@@ -1067,9 +1067,9 @@ def LoadEntranceNames(reload_=False):
                 id_, name = line.strip().split(':')
                 names[int(id_)] = name
 
-    globals_.EntranceTypeNames = []
+    globals_.EntranceTypeNames = collections.OrderedDict()
     for idx in names:
-        globals_.EntranceTypeNames.append(globals_.trans.string('EntranceDataEditor', 28, '[id]', idx, '[name]', names[idx]))
+        globals_.EntranceTypeNames[idx] = globals_.trans.string('EntranceDataEditor', 28, '[id]', idx, '[name]', names[idx])
 
 
 def LoadTilesetInfo(reload_=False):