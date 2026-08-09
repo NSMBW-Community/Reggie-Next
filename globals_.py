af4d583a75606e9340448d6630bdaafd657ea802
B1Here <58891561+B1Here@users.noreply.github.com>
2026-08-09 11:15:27 +0200

    Organise/categorise globals_.py

--- globals_.py
+++ globals_.py
@@ -13,12 +13,45 @@ from tiles import ObjectDef, TilesetTile
 from translation import ReggieTranslation
 from ui import ReggieTheme
 
-Area: AreaType = AreaType.DummyArea()
+# Reggie / UI
 AutoSaveData = b''
 AutoSaveDirty = False
 AutoSavePath = ''
 BgANames: list[str] = []
 BgBNames: list[str] = []
+CursorMode = 0
+DarkMode = False
+EntranceTypeNames: OrderedDict[int, str] = OrderedDict()
+ErrMsg = ''
+FirstStageFilename: str | None = None
+Initializing = False
+LevelNames: tuple[str, ...] = ()
+MusicInfo: dict[str, str] = {}
+NumberFont: QtGui.QFont | None = None
+ObjDesc: dict[int, str] = {}
+ReggieID = 'Reggie! Next Level Editor by Treeki, Tempus and RoadrunnerWMC'
+ReggieVersionFloat = 4.11
+ReggieVersionShort = 'v4.11.0'
+RestoredFromAutoSave = False
+TilesetTabPos = 0
+UseFullFilepath = False
+
+# Menu
+EditActions: tuple[MenuAction, ...] = ()
+FileActions: tuple[MenuAction, ...] = ()
+HelpActions: tuple[MenuAction, ...] = ()
+SettingsActions: tuple[MenuAction, ...] = ()
+ViewActions: tuple[MenuAction, ...] = ()
+
+# Keybinds
+# TODO may turn into a class in classlib.py
+FileKeybinds: dict[str, tuple[QtCore.QKeyCombination | str | None, str | None]]
+EditKeybinds: dict[str, tuple[QtCore.QKeyCombination | str | None, str | None]]
+ViewKeybinds: dict[str, tuple[QtCore.QKeyCombination | str | None, str | None]]
+SettingsKeybinds: dict[str, tuple[QtCore.QKeyCombination | str | None, str | None]]
+HelpKeybinds: dict[str, tuple[QtCore.QKeyCombination | str | None, str | None]]
+
+# Canvas / Editor
 BoundsDrawn = False
 CollisionsShown = False
 CommentsFrozen = False
@@ -27,35 +60,39 @@ CurrentLayer = 1
 CurrentObject = -1
 CurrentPaintType = 0
 CurrentSprite = -1
-Dirty = False
-DirtyOverride = 0
 DrawEntIndicators = False
-EditActions: tuple[MenuAction, ...] = ()
-EnablePadding = False
-EntranceTypeNames: OrderedDict[int, str] = OrderedDict()
 EntrancesFrozen = False
 EntrancesShown = True
-ErrMsg = ''
-FileActions: tuple[MenuAction, ...] = ()
-FileExtentions = ('.arc', '.arc.LH', '.arc.LZ')
 GridType: Literal['grid', 'checker'] | None = None
-HideResetSpritedata = False
-HelpActions: tuple[MenuAction, ...] = ()
-Initializing = False
 InsertPathNode = False
 Layer0Shown = True
 Layer1Shown = True
 Layer2Shown = True
-Level: AbstractLevel # Uninitialized on purpose. It's never accessed before being written to. Reduces redudant "is None" checks.
-LevelNames: tuple[str, ...] = ()
 LocationsFrozen = False
 LocationsShown = True
-MusicInfo: dict[str, str] = {}
-NumberFont: QtGui.QFont | None = None
-NumSprites = 0
-ObjDesc: dict[int, str] = {}
-ObjectDefinitions: list[list[ObjectDef | None]] = [] # 4 tilesets
 ObjectsFrozen = False
+PathsFrozen = False
+PathsShown = True
+PlaceObjectsAtFullSize = True
+RealViewEnabled = False
+SpriteImagesShown = True
+SpritesFrozen = False
+SpritesShown = True
+TilesetsAnimating = False
+UseRoundedRectangles = True
+
+# Level
+Area: AreaType = AreaType.DummyArea()
+Dirty = False
+DirtyOverride = 0
+EnablePadding = False
+FileExtentions = ('.arc', '.arc.LH', '.arc.LZ')
+Level: AbstractLevel # Uninitialized on purpose. It's never accessed before being written to. Reduces redudant "is None" checks.
+PaddingLength = 0
+ZoneThemeValues: list[str] = []
+
+# Tilesets
+ObjectDefinitions: list[list[ObjectDef | None]] = [] # 4 tilesets
 OverriddenTilesets: dict[str, set[str]] = {
     "Pa0": set(),
     "no-Pa0": set(),
@@ -70,45 +107,21 @@ OverrideSnapping = False
 Overrides: list[TilesetTile | None] = [] # 320 tiles, this is put into Tiles usually
 Overrides_safe: list[TilesetTile | None] = []
 OVERRIDE_UNKNOWN = 0
-PaddingLength = 0
-PathsFrozen = False
-PathsShown = True
-PlaceObjectsAtFullSize = True
-RealViewEnabled = False
-ReggieID = 'Reggie! Next Level Editor by Treeki, Tempus and RoadrunnerWMC'
-ReggieVersionFloat = 4.11
-ReggieVersionShort = 'v4.11.0'
-ResetDataWhenHiding = False
-RestoredFromAutoSave = False
-SettingsActions: tuple[MenuAction, ...] = ()
-SpriteCategories: list[SpriteCategory] = []
-SpriteImagesShown = True
-SpriteListData: list[list[int]] = []
-SpritesFrozen = False
-SpritesShown = True
-Sprites: list[SpriteDefinition] = []
 Tiles: dict[int, list[TilesetTile | None]] = {} # 0x200 tiles per tileset, plus 64 for each type of override
 TilesetAnimTimer: QtCore.QTimer | None = None
 TilesetFilesLoaded: list[str | None] = [None for _ in range(4)] # should always have exactly 4 entries
 TilesetInfo: dict[str, dict[int, RandTileSelection]] = {}
 TilesetNames: list[TilesetCategory] = [TilesetCategory() for _ in range(4)] # should always have exactly 4 entries
-TilesetsAnimating = False
-ViewActions: tuple[MenuAction, ...] = ()
-ZoneThemeValues: list[str] = []
-UseRoundedRectangles = True
-DarkMode = False
-UseFullFilepath = False
-FirstStageFilename: str | None = None
-CursorMode = 0
-TilesetTabPos = 0
 
-FileKeybinds: dict[str, tuple[QtCore.QKeyCombination | str | None, str | None]] # TODO may turn into a class in classlib.py
-EditKeybinds: dict[str, tuple[QtCore.QKeyCombination | str | None, str | None]]
-ViewKeybinds: dict[str, tuple[QtCore.QKeyCombination | str | None, str | None]]
-SettingsKeybinds: dict[str, tuple[QtCore.QKeyCombination | str | None, str | None]]
-HelpKeybinds: dict[str, tuple[QtCore.QKeyCombination | str | None, str | None]]
+# Sprites
+HideResetSpritedata = False
+NumSprites = 0
+ResetDataWhenHiding = False
+SpriteCategories: list[SpriteCategory] = []
+SpriteListData: list[list[int]] = []
+Sprites: list[SpriteDefinition] = []
 
-# Config settings
+# Game patch config settings
 DispConnectedPipeDir = False
 SpecialEventSpriteID = 0
 AllowSizeHacks = False
