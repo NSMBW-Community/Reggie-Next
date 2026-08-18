26a924ff04bd63cb7d6cb030a99179a5375856fd
mandy <67531582+MandyIGuess@users.noreply.github.com>
2026-08-18 19:18:51 -0400

    Fix outdated info in the batch script

--- run-python.bat
+++ run-python.bat
@@ -5,9 +5,9 @@ REM Thanks to LifeMushroom for the idea.
 REM Redone by Danster64.
 REM
 REM This only works if:
-REM - you have Python 3.5+
+REM - you have Python 3.12+
 REM - you have Python added to PATH
-REM - you installed all the other prerequisites (see readme.txt)
+REM - you installed all the other prerequisites (see README.md)
 REM - this file is in the same folder as reggie.py
 REM - you're on Windows
 REM
